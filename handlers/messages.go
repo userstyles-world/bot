432239c65a076439604a79904fd830ca3053f619
vednoc <vednoc@protonmail.com>
2021-12-16 15:31:01 +0100

    feat(cmd): add a 'report' command

--- handlers/messages.go
+++ handlers/messages.go
@@ -27,8 +27,31 @@ func OnMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	switch args[0] {
 	case "ping":
 		_, err = s.ChannelMessageSend(m.ChannelID, "Pong!")
+
+	case "report":
+		fmt.Printf("%#v\n", args)
+
+		// Check for empty reports.
+		if len(args) == 1 {
+			s.ChannelMessageSend(m.ChannelID, "Please fill out information about your report.")
+			break
+		}
+
+		// Delete the report message.
+		if err = s.ChannelMessageDelete(m.ChannelID, m.ID); err != nil {
+			break
+		}
+
+		content := strings.Join(args[1:], " ")
+		report := fmt.Sprintf("Report by %v:\n%v", m.Author.Mention(), content)
+		_, err = s.ChannelMessageSend(utils.ReportChannelID, report)
+		if err != nil {
+			break
+		}
+
 	case "help":
 		_, err = s.ChannelMessageSend(m.ChannelID, "`ping` - pings the bot\n`uptime` - shows bot uptime\n`deploy <branch>` deploy the server to that branch's current commit\n`help` - shows this message")
+
 	case "uptime":
 		uptime := time.Since(utils.LastUptime).Round(time.Second).String()
 		if utils.IsDown {
@@ -36,6 +59,7 @@ func OnMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 		} else {
 			_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Server has been **online** for %s.", uptime))
 		}
+
 	case "deploy":
 		if len(args) != 2 {
 			_, err = s.ChannelMessageSend(m.ChannelID, "Use the syntax: `deploy <branch>`")
@@ -77,6 +101,7 @@ func OnMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		_, err = s.ChannelMessageSend(m.ChannelID, "I'm happy REEE, "+gifURL)
 	}
+
 	if err != nil {
 		log.Println(err)
 	}
--- utils/hardcoded.go
+++ utils/hardcoded.go
@@ -6,6 +6,7 @@ package utils
 var (
 	AnnouncementsID = "872596505222078494"
 	StatusChannelID = "821455365274075136"
+	ReportChannelID = "921044291613257769"
 	Prefix          = "&"
 	AdminRoleID     = "821440790412787752"
 )
